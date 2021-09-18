4bba84a2e4f2e3dc911f297baf79d5a0b4c0c49b
gadgetfather <kamblea635@gmail.com>
2021-09-18 12:42:52 +0530

    added clickhandler

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ var ogText = document.querySelector('#og-text')
 var translateBtn = document.querySelector('#translate-btn')
 var outputEl = document.querySelector('#output')
 
+
+function clickHandler(){
+    console.log("clicked")
+}
+
+translateBtn.addEventListener('click',clickHandler)
