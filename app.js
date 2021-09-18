b1dd8abab1c3bb3d42e65ce67e08fe70c20f5c26
gadgetfather <kamblea635@gmail.com>
2021-09-18 13:32:43 +0530

    added server

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-var ogText = document.querySelector('#og-text')
-var translateBtn = document.querySelector('#translate-btn')
-var outputEl = document.querySelector('#output')
+var btnTranslate = document.querySelector("#translate-btn");
+var txtInput = document.querySelector("#og-text")
+var outputDiv = document.querySelector("#output")
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
 
-function clickHandler(){
-    console.log("clicked")
+function getTranslationUrl(text) {
+    return serverURL + "?" +"text="+ text
 }
 
-translateBtn.addEventListener('click',clickHandler)
+function errorHandler(error){
+    console.log("error is here", error)
+    alert("Something wrong with server try again later!")
+}
+
+
+
+function clickHandler() {
+
+    //outputDiv.innerText = "sdasdas "+ txtInput.value;
+    var Inputtext = txtInput.value
+    fetch(getTranslationUrl(Inputtext))
+        .then(response => response.json())
+        .then(json => {var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText
+            })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
